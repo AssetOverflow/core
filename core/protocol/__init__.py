6701a63b589400bd5c988e72d999d721e69b5c2e
Shay <assetoverflow@icloud.com>
2026-05-24 20:48:48 -0700

    feat(protocol): export CTP constructors

--- core/protocol/__init__.py
+++ core/protocol/__init__.py
@@ -1,7 +1,19 @@
 """CORE Trace Protocol v0 package."""
 
 from .canonical import canonical_bytes, canonical_hash, canonicalize
-from .envelope import CtpActor, CtpEnvelope, CtpEpistemic, CtpInvariant, CtpPayload, CtpProof, CtpStateRef
+from .envelope import CtpEnvelope
+from .events import (
+    evidence_observed,
+    invariant_checked,
+    learning_proposal_created,
+    tool_invocation_completed,
+    tool_invocation_requested,
+    turn_completed,
+    turn_refused,
+    turn_requested,
+    verdict_assigned,
+)
+from .types import CtpActor, CtpEpistemic, CtpInvariant, CtpPayload, CtpProof, CtpStateRef
 
 __all__ = [
     "CtpActor",
@@ -14,4 +26,13 @@ __all__ = [
     "canonical_bytes",
     "canonical_hash",
     "canonicalize",
+    "evidence_observed",
+    "invariant_checked",
+    "learning_proposal_created",
+    "tool_invocation_completed",
+    "tool_invocation_requested",
+    "turn_completed",
+    "turn_refused",
+    "turn_requested",
+    "verdict_assigned",
 ]
